6c2254e544ec7f0cc4cc72188531c6c3575b3fce
NathanFallet <contact@nathanfallet.me>
2025-12-09 16:40:17 +0100

    add missing targets

--- nextjs/build.gradle.kts
+++ nextjs/build.gradle.kts
@@ -36,6 +36,29 @@ mavenPublishing {
 }
 
 kotlin {
+    // Tiers are in accordance with <https://kotlinlang.org/docs/native-target-support.html>
+    // Tier 1
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+
+    // Tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+    iosArm64()
+
+    // Tier 3
+    mingwX64()
+    watchosDeviceArm64()
+    
     // jvm & js
     jvmToolchain(21)
     jvm {
